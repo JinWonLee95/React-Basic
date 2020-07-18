ea16110ec5f9ef1fae9510e35ee868e28b7f0dda
JinWonLee95 <dlwlsdnjs456@naver.com>
2020-07-18 16:57:17 +0900

    event

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TOC from "./components/TOC";
 import Content from "./components/Content";
 import Subject from "./components/Subject";
 import './App.css';
+import { configure } from '@testing-library/react';
 
 
 class App extends Component {
@@ -10,7 +11,9 @@ class App extends Component {
     // 컴퍼넌트가 실행될때 render보다 먼저 실행돼서 초기화 시키려면 여기 안에 작성
     super(props);
     this.state = {
+      mode: 'welcome',
       subject:{title:'WEB', sub:'World Wide Web'},
+      welcome:{title:'Welcome', desc:'Hello, React!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -19,14 +22,29 @@ class App extends Component {
     }
   }
 
+  //props나 state가 바뀌면 render가 다시 호출된다(화면이 새로고침된다)
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} 
-        sub={this.state.subject.sub}>
+        <Subject
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'read'});
+          }.bind(this)}
+        >
         </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
